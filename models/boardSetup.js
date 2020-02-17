f72d2d519b4c8154f7a756ffe048024683312a5a
methoil <mateev.br@gmail.com>
2020-02-16 23:57:28 -0500

    refator - remove unused code

--- models/boardSetup.js
+++ models/boardSetup.js
@@ -11,7 +11,7 @@ var pieceTypes;
     pieceTypes["levy"] = "levy";
     pieceTypes["archer"] = "archer";
     pieceTypes["cataphract"] = "cataphract";
-    pieceTypes["hoplite"] = "hoplite";
+    pieceTypes["legion"] = "legion";
     pieceTypes["chariot"] = "chariot";
     pieceTypes["warElephant"] = "warElephant";
     pieceTypes["centaur"] = "centaur";
@@ -21,14 +21,14 @@ var pieceTypes;
 exports.pieceDefs = [
     {
         pieceType: 'levy',
-        health: 2,
+        health: 3,
     },
     {
         pieceType: 'archer',
         health: 2,
     },
     {
-        pieceType: 'hoplite',
+        pieceType: 'legion',
         health: 8,
     },
     {
@@ -37,7 +37,7 @@ exports.pieceDefs = [
     },
     {
         pieceType: 'chariot',
-        health: 12,
+        health: 10,
     },
     {
         pieceType: 'warElephant',
@@ -64,79 +64,88 @@ function generateNewBoard() {
         var rowArray = new Array(BOARD_WIDTH).fill(null);
         // phrygian side
         if (x === 0) {
-            rowArray[5] = makePhrygianPiece('cataphract');
+            //d
+            rowArray[4] = makePhrygianPiece('cataphract');
             rowArray[rowArray.length - 5] = makePhrygianPiece('cataphract');
-            rowArray[7] = makePhrygianPiece('general');
+            rowArray[8] = makePhrygianPiece('general');
             rowArray[9] = makePhrygianPiece('warElephant');
-            rowArray[2] = makePhrygianPiece('archer');
-            rowArray[10] = makePhrygianPiece('archer');
-            rowArray[13] = makePhrygianPiece('archer');
+            rowArray[6] = makePhrygianPiece('chariot');
+            rowArray[rowArray.length - 7] = makePhrygianPiece('chariot');
+            rowArray[2] = makePhrygianPiece('centaur');
+            rowArray[rowArray.length - 3] = makePhrygianPiece('centaur');
         }
         else if (x === 1) {
-            rowArray[6] = makePhrygianPiece('chariot');
-            rowArray[rowArray.length - 6] = makePhrygianPiece('chariot');
-            rowArray[4] = makePhrygianPiece('archer');
-            rowArray[8] = makePhrygianPiece('archer');
-            rowArray[12] = makePhrygianPiece('archer');
-            rowArray[2] = makePhrygianPiece('centaur');
-            rowArray[rowArray.length - 2] = makePhrygianPiece('centaur');
+            rowArray.fill(makePhrygianPiece('levy'), 0, 3);
+            rowArray.fill(makePhrygianPiece('levy'), rowArray.length - 3, rowArray.length);
+            rowArray.fill(makePhrygianPiece('legion'), 4, 6);
+            rowArray.fill(makePhrygianPiece('legion'), 12, 14);
+            rowArray.fill(makePhrygianPiece('archer'), 7, 11);
         }
         else if (x === 2) {
-            rowArray[1] = makePhrygianPiece('lightCavalry');
-            rowArray.fill(makePhrygianPiece('hoplite'), 3, 6);
-            rowArray.fill(makePhrygianPiece('hoplite'), 7, 10);
-            rowArray.fill(makePhrygianPiece('hoplite'), 11, 14);
-            rowArray[rowArray.length - 2] = makePhrygianPiece('lightCavalry');
+            rowArray[2] = makePhrygianPiece('lightCavalry');
+            rowArray[4] = makePhrygianPiece('levy');
+            rowArray[rowArray.length - 3] = makePhrygianPiece('lightCavalry');
+            rowArray[rowArray.length - 5] = makePhrygianPiece('levy');
+            rowArray.fill(makePhrygianPiece('legion'), 6, 12);
         }
         else if (x === 3) {
-            rowArray.fill(makePhrygianPiece('levy'), 2, 4);
-            rowArray.fill(makePhrygianPiece('levy'), 7, 11);
-            rowArray.fill(makePhrygianPiece('levy'), 12, 14);
+            rowArray[2] = makePhrygianPiece('chariot');
+            rowArray.fill(makePhrygianPiece('archer'), 3, 4);
+            rowArray.fill(makePhrygianPiece('archer'), 5, 6);
+            // rowArray.fill(makePhrygianPiece('levy'), 7, rowArray.length - 7);
+            rowArray[rowArray.length - 6] = makePhrygianPiece('archer');
+            rowArray[rowArray.length - 3] = makePhrygianPiece('chariot');
+            rowArray[rowArray.length - 4] = makePhrygianPiece('archer');
+            rowArray[0] = makePhrygianPiece('lightCavalry');
+            rowArray[rowArray.length - 1] = makePhrygianPiece('lightCavalry');
         }
         else if (x === 4) {
-            rowArray[4] = makePhrygianPiece('lightCavalry');
-            rowArray.fill(makePhrygianPiece('levy'), 2, 6);
-            rowArray.fill(makePhrygianPiece('levy'), 8, 10);
-            rowArray.fill(makePhrygianPiece('levy'), 10, 14);
-            rowArray[rowArray.length - 5] = makePhrygianPiece('lightCavalry');
+            rowArray.fill(makePhrygianPiece('levy'), 0, 1);
+            rowArray.fill(makePhrygianPiece('levy'), 3, 7);
+            rowArray.fill(makePhrygianPiece('levy'), rowArray.length - 7, rowArray.length - 3);
+            rowArray.fill(makePhrygianPiece('levy'), rowArray.length - 1, rowArray.length);
         }
         // hittite side
         else if (x === BOARD_HEIGHT - 1) {
-            rowArray[5] = makeHittitePiece('cataphract');
+            rowArray[4] = makeHittitePiece('cataphract');
             rowArray[rowArray.length - 5] = makeHittitePiece('cataphract');
-            rowArray[7] = makeHittitePiece('general');
+            rowArray[8] = makeHittitePiece('general');
             rowArray[9] = makeHittitePiece('warElephant');
-            rowArray[2] = makeHittitePiece('archer');
-            rowArray[10] = makeHittitePiece('archer');
-            rowArray[13] = makeHittitePiece('archer');
+            rowArray[6] = makeHittitePiece('chariot');
+            rowArray[rowArray.length - 7] = makeHittitePiece('chariot');
+            rowArray[2] = makeHittitePiece('centaur');
+            rowArray[rowArray.length - 3] = makeHittitePiece('centaur');
         }
         else if (x === BOARD_HEIGHT - 2) {
-            rowArray[6] = makeHittitePiece('chariot');
-            rowArray[rowArray.length - 6] = makeHittitePiece('chariot');
-            rowArray[4] = makeHittitePiece('archer');
-            rowArray[8] = makeHittitePiece('archer');
-            rowArray[12] = makeHittitePiece('archer');
-            rowArray[2] = makeHittitePiece('centaur');
-            rowArray[rowArray.length - 2] = makeHittitePiece('centaur');
+            rowArray.fill(makeHittitePiece('levy'), 0, 3);
+            rowArray.fill(makeHittitePiece('levy'), rowArray.length - 3, rowArray.length);
+            rowArray.fill(makeHittitePiece('legion'), 4, 6);
+            rowArray.fill(makeHittitePiece('legion'), 12, 14);
+            rowArray.fill(makeHittitePiece('archer'), 7, 11);
         }
         else if (x === BOARD_HEIGHT - 3) {
-            rowArray[1] = makeHittitePiece('lightCavalry');
-            rowArray.fill(makeHittitePiece('hoplite'), 3, 6);
-            rowArray.fill(makeHittitePiece('hoplite'), 7, 10);
-            rowArray.fill(makeHittitePiece('hoplite'), 11, 14);
-            rowArray[rowArray.length - 2] = makeHittitePiece('lightCavalry');
+            rowArray[2] = makeHittitePiece('lightCavalry');
+            rowArray[4] = makeHittitePiece('levy');
+            rowArray[rowArray.length - 3] = makeHittitePiece('lightCavalry');
+            rowArray[rowArray.length - 5] = makeHittitePiece('levy');
+            rowArray.fill(makeHittitePiece('legion'), 6, 12);
         }
         else if (x === BOARD_HEIGHT - 4) {
-            rowArray.fill(makeHittitePiece('levy'), 2, 4);
-            rowArray.fill(makeHittitePiece('levy'), 7, 11);
-            rowArray.fill(makeHittitePiece('levy'), 12, 14);
+            rowArray[2] = makeHittitePiece('chariot');
+            rowArray.fill(makeHittitePiece('archer'), 3, 4);
+            rowArray.fill(makeHittitePiece('archer'), 5, 6);
+            // rowArray.fill(makeHittitePiece('levy'), 7, rowArray.length - 7);
+            rowArray[rowArray.length - 6] = makeHittitePiece('archer');
+            rowArray[rowArray.length - 3] = makeHittitePiece('chariot');
+            rowArray[rowArray.length - 4] = makeHittitePiece('archer');
+            rowArray[0] = makeHittitePiece('lightCavalry');
+            rowArray[rowArray.length - 1] = makeHittitePiece('lightCavalry');
         }
         else if (x === BOARD_HEIGHT - 5) {
-            rowArray[4] = makeHittitePiece('lightCavalry');
-            rowArray.fill(makeHittitePiece('levy'), 2, 6);
-            rowArray.fill(makeHittitePiece('levy'), 8, 10);
-            rowArray.fill(makeHittitePiece('levy'), 10, 14);
-            rowArray[rowArray.length - 5] = makeHittitePiece('lightCavalry');
+            rowArray.fill(makeHittitePiece('levy'), 0, 1);
+            rowArray.fill(makeHittitePiece('levy'), 3, 7);
+            rowArray.fill(makeHittitePiece('levy'), rowArray.length - 7, rowArray.length - 3);
+            rowArray.fill(makeHittitePiece('levy'), rowArray.length - 1, rowArray.length);
         }
         boardState.push(rowArray);
     }
--- models/boardSetup.ts
+++ models/boardSetup.ts
@@ -9,7 +9,7 @@ export enum pieceTypes {
   levy = 'levy',
   archer = 'archer',
   cataphract = 'cataphract',
-  hoplite = 'hoplite',
+  legion = 'legion',
   chariot = 'chariot',
   warElephant = 'warElephant',
   centaur = 'centaur',
@@ -29,14 +29,14 @@ export interface IPieceMeta extends IPieceDef {
 export const pieceDefs: IPieceDef[] = [
   {
     pieceType: 'levy',
-    health: 2,
+    health: 3,
   },
   {
     pieceType: 'archer',
     health: 2,
   },
   {
-    pieceType: 'hoplite',
+    pieceType: 'legion',
     health: 8,
   },
   {
@@ -45,7 +45,7 @@ export const pieceDefs: IPieceDef[] = [
   },
   {
     pieceType: 'chariot',
-    health: 12,
+    health: 10,
   },
   {
     pieceType: 'warElephant',
@@ -74,73 +74,82 @@ export function generateNewBoard(): Array<IPieceMeta | null>[] {
     const rowArray: Array<IPieceMeta | null> = new Array(BOARD_WIDTH).fill(null);
     // phrygian side
     if (x === 0) {
-      rowArray[5] = makePhrygianPiece('cataphract');
+      //d
+      rowArray[4] = makePhrygianPiece('cataphract');
       rowArray[rowArray.length - 5] = makePhrygianPiece('cataphract');
-      rowArray[7] = makePhrygianPiece('general');
+      rowArray[8] = makePhrygianPiece('general');
       rowArray[9] = makePhrygianPiece('warElephant');
-
-      rowArray[2] = makePhrygianPiece('archer');
-      rowArray[10] = makePhrygianPiece('archer');
-      rowArray[13] = makePhrygianPiece('archer');
-    } else if (x === 1) {
       rowArray[6] = makePhrygianPiece('chariot');
-      rowArray[rowArray.length - 6] = makePhrygianPiece('chariot');
-      rowArray[4] = makePhrygianPiece('archer');
-      rowArray[8] = makePhrygianPiece('archer');
-      rowArray[12] = makePhrygianPiece('archer');
+      rowArray[rowArray.length - 7] = makePhrygianPiece('chariot');
       rowArray[2] = makePhrygianPiece('centaur');
-      rowArray[rowArray.length - 2] = makePhrygianPiece('centaur');
+      rowArray[rowArray.length - 3] = makePhrygianPiece('centaur');
+    } else if (x === 1) {
+      rowArray.fill(makePhrygianPiece('levy'), 0, 3);
+      rowArray.fill(makePhrygianPiece('levy'), rowArray.length - 3, rowArray.length);
+
+      rowArray.fill(makePhrygianPiece('legion'), 4, 6);
+      rowArray.fill(makePhrygianPiece('legion'), 12, 14);
+      rowArray.fill(makePhrygianPiece('archer'), 7, 11);
     } else if (x === 2) {
-      rowArray[1] = makePhrygianPiece('lightCavalry');
-      rowArray.fill(makePhrygianPiece('hoplite'), 3, 6);
-      rowArray.fill(makePhrygianPiece('hoplite'), 7, 10);
-      rowArray.fill(makePhrygianPiece('hoplite'), 11, 14);
-      rowArray[rowArray.length - 2] = makePhrygianPiece('lightCavalry');
+      rowArray[2] = makePhrygianPiece('lightCavalry');
+      rowArray[4] = makePhrygianPiece('levy');
+      rowArray[rowArray.length - 3] = makePhrygianPiece('lightCavalry');
+      rowArray[rowArray.length - 5] = makePhrygianPiece('levy');
+      rowArray.fill(makePhrygianPiece('legion'), 6, 12);
     } else if (x === 3) {
-      rowArray.fill(makePhrygianPiece('levy'), 2, 4);
-      rowArray.fill(makePhrygianPiece('levy'), 7, 11);
-      rowArray.fill(makePhrygianPiece('levy'), 12, 14);
+      rowArray[2] = makePhrygianPiece('chariot');
+      rowArray.fill(makePhrygianPiece('archer'), 3, 4);
+      rowArray.fill(makePhrygianPiece('archer'), 5, 6);
+      // rowArray.fill(makePhrygianPiece('levy'), 7, rowArray.length - 7);
+      rowArray[rowArray.length - 6] = makePhrygianPiece('archer');
+      rowArray[rowArray.length - 3] = makePhrygianPiece('chariot');
+      rowArray[rowArray.length - 4] = makePhrygianPiece('archer');
+      rowArray[0] = makePhrygianPiece('lightCavalry');
+      rowArray[rowArray.length - 1] = makePhrygianPiece('lightCavalry');
     } else if (x === 4) {
-      rowArray[4] = makePhrygianPiece('lightCavalry');
-      rowArray.fill(makePhrygianPiece('levy'), 2, 6);
-      rowArray.fill(makePhrygianPiece('levy'), 8, 10);
-      rowArray.fill(makePhrygianPiece('levy'), 10, 14);
-      rowArray[rowArray.length - 5] = makePhrygianPiece('lightCavalry');
+      rowArray.fill(makePhrygianPiece('levy'), 0, 1);
+      rowArray.fill(makePhrygianPiece('levy'), 3, 7);
+      rowArray.fill(makePhrygianPiece('levy'), rowArray.length - 7, rowArray.length - 3);
+      rowArray.fill(makePhrygianPiece('levy'), rowArray.length - 1, rowArray.length);
     }
     // hittite side
     else if (x === BOARD_HEIGHT - 1) {
-      rowArray[5] = makeHittitePiece('cataphract');
+      rowArray[4] = makeHittitePiece('cataphract');
       rowArray[rowArray.length - 5] = makeHittitePiece('cataphract');
-      rowArray[7] = makeHittitePiece('general');
+      rowArray[8] = makeHittitePiece('general');
       rowArray[9] = makeHittitePiece('warElephant');
-
-      rowArray[2] = makeHittitePiece('archer');
-      rowArray[10] = makeHittitePiece('archer');
-      rowArray[13] = makeHittitePiece('archer');
-    } else if (x === BOARD_HEIGHT - 2) {
       rowArray[6] = makeHittitePiece('chariot');
-      rowArray[rowArray.length - 6] = makeHittitePiece('chariot');
-      rowArray[4] = makeHittitePiece('archer');
-      rowArray[8] = makeHittitePiece('archer');
-      rowArray[12] = makeHittitePiece('archer');
+      rowArray[rowArray.length - 7] = makeHittitePiece('chariot');
       rowArray[2] = makeHittitePiece('centaur');
-      rowArray[rowArray.length - 2] = makeHittitePiece('centaur');
+      rowArray[rowArray.length - 3] = makeHittitePiece('centaur');
+    } else if (x === BOARD_HEIGHT - 2) {
+      rowArray.fill(makeHittitePiece('levy'), 0, 3);
+      rowArray.fill(makeHittitePiece('levy'), rowArray.length - 3, rowArray.length);
+
+      rowArray.fill(makeHittitePiece('legion'), 4, 6);
+      rowArray.fill(makeHittitePiece('legion'), 12, 14);
+      rowArray.fill(makeHittitePiece('archer'), 7, 11);
     } else if (x === BOARD_HEIGHT - 3) {
-      rowArray[1] = makeHittitePiece('lightCavalry');
-      rowArray.fill(makeHittitePiece('hoplite'), 3, 6);
-      rowArray.fill(makeHittitePiece('hoplite'), 7, 10);
-      rowArray.fill(makeHittitePiece('hoplite'), 11, 14);
-      rowArray[rowArray.length - 2] = makeHittitePiece('lightCavalry');
+      rowArray[2] = makeHittitePiece('lightCavalry');
+      rowArray[4] = makeHittitePiece('levy');
+      rowArray[rowArray.length - 3] = makeHittitePiece('lightCavalry');
+      rowArray[rowArray.length - 5] = makeHittitePiece('levy');
+      rowArray.fill(makeHittitePiece('legion'), 6, 12);
     } else if (x === BOARD_HEIGHT - 4) {
-      rowArray.fill(makeHittitePiece('levy'), 2, 4);
-      rowArray.fill(makeHittitePiece('levy'), 7, 11);
-      rowArray.fill(makeHittitePiece('levy'), 12, 14);
+      rowArray[2] = makeHittitePiece('chariot');
+      rowArray.fill(makeHittitePiece('archer'), 3, 4);
+      rowArray.fill(makeHittitePiece('archer'), 5, 6);
+      // rowArray.fill(makeHittitePiece('levy'), 7, rowArray.length - 7);
+      rowArray[rowArray.length - 6] = makeHittitePiece('archer');
+      rowArray[rowArray.length - 3] = makeHittitePiece('chariot');
+      rowArray[rowArray.length - 4] = makeHittitePiece('archer');
+      rowArray[0] = makeHittitePiece('lightCavalry');
+      rowArray[rowArray.length - 1] = makeHittitePiece('lightCavalry');
     } else if (x === BOARD_HEIGHT - 5) {
-      rowArray[4] = makeHittitePiece('lightCavalry');
-      rowArray.fill(makeHittitePiece('levy'), 2, 6);
-      rowArray.fill(makeHittitePiece('levy'), 8, 10);
-      rowArray.fill(makeHittitePiece('levy'), 10, 14);
-      rowArray[rowArray.length - 5] = makeHittitePiece('lightCavalry');
+      rowArray.fill(makeHittitePiece('levy'), 0, 1);
+      rowArray.fill(makeHittitePiece('levy'), 3, 7);
+      rowArray.fill(makeHittitePiece('levy'), rowArray.length - 7, rowArray.length - 3);
+      rowArray.fill(makeHittitePiece('levy'), rowArray.length - 1, rowArray.length);
     }
 
     boardState.push(rowArray);
